be0b9eaab0c8031dc2de834bce9f609468465606
Rott Kitchell <r.kitchell815@gmail.com>
2024-05-23 01:37:56 -0500

    initail framework

---
+++ BCtoMondayV2/test/b_cto_monday_v2.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const BCtoMondayV2 = require('../lib/b_cto_monday_v2-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/b_cto_monday_v2-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new BCtoMondayV2.BCtoMondayV2Stack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
